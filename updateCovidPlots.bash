881a12a4b45de74de171162f62cff5fae20409f0
fkzack <github@fzack.com>
2020-06-01 08:45:01 -0700

    update plots

--- updateCovidPlots.bash
+++ updateCovidPlots.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/fred/R/covid/covid
-R -e "rmarkdown::render('CovidPlots.rmd')"
+#do this via an r script to allow setting path to pandoc when called from crontab
+#R -e "rmarkdown::render('CovidPlots.rmd')"
+Rscript updateCovidPlots.r
 git add .
 git commit -m"update plots"
 git push
---
+++ updateCovidPlots.r
@@ -0,0 +1,3 @@
+# generate all of the covid plots, setting the pandoc path so it can be seen when run from crontab
+Sys.setenv(RSTUDIO_PANDOC = "/usr/local/bin/pandoc")
+rmarkdown::render('CovidPlots.rmd')
