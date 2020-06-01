c98a88ceb50b5cf5f157314ac24c3ac9da43c761
fkzack <github@fzack.com>
2020-06-01 09:25:56 -0700

    fix pandoc path when called from cron job

--- updateCovidPlots.bash
+++ updateCovidPlots.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
+# generate and upload all of the covid plots
 cd /home/fred/R/covid/covid
-#do this via an r script to allow setting path to pandoc when called from crontab
+#Explicitly set pandoc path so that this will work when called from cron job
 R -e "Sys.setenv(RSTUDIO_PANDOC = '/usr/lib/rstudio/bin/pandoc'); rmarkdown::render('CovidPlots.rmd')"
-#Rscript updateCovidPlots.r
 git add .
 git commit -m"update plots"
 git push
--- updateCovidPlots.r
+++
@@ -1,4 +0,0 @@
-# generate all of the covid plots, setting the pandoc path so it can be seen when run from crontab
-library(rmarkdown)
-Sys.setenv(RSTUDIO_PANDOC = "/usr/local/bin/pandoc")
-rmarkdown::render('CovidPlots.rmd')
